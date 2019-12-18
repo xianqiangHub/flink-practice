3a4539b2a53856dcca5c9a2bb39dfa36a4725c69
xianqiang <982535901@qq.com>
2019-12-18 10:19:44 +0800

    pom

---
+++ flink-sql-submit/src/main/java/com/bigdata/example/Test.java
@@ -0,0 +1,12 @@
+package com.bigdata.example;
+
+/**
+ * flinksql使用的优化  https://help.aliyun.com/document_detail/98981.html?spm=a2c4g.11186623.6.614.86286d163E6NmE
+ *
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
