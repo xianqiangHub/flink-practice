5546ee54eb72b514f24c42615d154f8be7dd602b
xianqiang <982535901@qq.com>
2019-12-19 11:14:48 +0800

    jdbc

--- connector/src/main/java/com/bigdata/JDBC/MyJDBC.java
+++ connector/src/main/java/com/bigdata/JDBC/MyJDBC.java
@@ -1,11 +1,16 @@
 package com.bigdata.JDBC;
 
 /**
- *  一个是JDBC source 提高并行度。。
- *
- *  sink的话：
- *  一种 实现sinkfunction 里面自己写JDBC
- *  一种 使用使用jdbc写出  .writeUsingOutputFormat
+ * 一个是JDBC source 提高并行度。。
+ * Flink在读取JDBC表时，为了加快速度，通常可以并发的方式读取，只需要增加以下几个参数：
+ * 'connector.read.partition.column'='id',
+ * 'connector.read.partition.lower-bound'='1',
+ * 'connector.read.partition.upper-bound'='1000',
+ * 'connector.read.partition.num'='10'
+ * <p>
+ * sink的话：
+ * 一种 实现sinkfunction 里面自己写JDBC
+ * 一种 使用使用jdbc写出  .writeUsingOutputFormat
  */
 public class MyJDBC {
 
