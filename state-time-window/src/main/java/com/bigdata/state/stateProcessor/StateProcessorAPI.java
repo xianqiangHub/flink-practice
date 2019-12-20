28e58c9051003b481c54a73c01898b27051aeb2c
xianqiang <982535901@qq.com>
2019-12-20 10:12:42 +0800

    state

---
+++ state-time-window/src/main/java/com/bigdata/state/stateProcessor/StateProcessorAPI.java
@@ -0,0 +1,15 @@
+package com.bigdata.state.stateProcessor;
+
+/**
+ * https://mp.weixin.qq.com/s/2bXrq9j4x_DcGie1gFGi5A
+ * <p>
+ * 在外部读入修改写入状态save point或checkpoint
+ * 用的DataSet实现，以后可能再移除
+ * checkpoint理解：可以理解为database，状态算子的uid理解为namespace，定义状态的名字可以理解为tablename
+ */
+public class StateProcessorAPI {
+
+    public static void main(String[] args) {
+//这块还不太理解，官网给出了API示例
+    }
+}
